fa9455eb803eb51e467c53d473914ad9c0805d9c
IDVAN3 <53148453+IDVAN3@users.noreply.github.com>
2021-02-20 01:21:09 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,169 @@
+
+'use strict'
+$(document).ready(function () {
+
+    /*ibg*/
+
+    function ibg() {
+        $.each($('.ibg'), function (index, val) {
+            if ($(this).find('img').length > 0) {
+                let src_img = $(this).find('img').attr('src');
+                $(this).css('backgroundImage', 'url("' + src_img + '")');
+            }
+        });
+    }
+
+    ibg();
+
+    /* === popup start === */
+
+    let unlock = true;
+      const timeout = 300;
+      const body = document.querySelector("body");
+      const lockPadding = document.querySelectorAll(".lock-padding");
+      
+      function bodyLock() {
+        const lockPaddingValue =
+          window.innerWidth - document.querySelector(".wrapper").offsetWidth + "px";
+        if (lockPadding.length > 0) {
+          for (let i = 0; i < lockPadding.length; i++) {
+            const el = lockPadding[i];
+            el.style.paddingRight = lockPaddingValue;
+          }
+        }
+        body.style.paddingRight = lockPaddingValue;
+        body.classList.add("lock");
+      
+        unlock = false;
+        setTimeout(function () {
+          unlock = true;
+        }, timeout);
+      }
+      
+      function bodyUnlock() {
+        setTimeout(function () {
+          for (let i = 0; i < lockPadding.length; i++) {
+            const el = lockPadding[i];
+            el.style.paddingRight = "0px";
+          }
+          body.style.paddingRight = "0px";
+          body.classList.remove("lock");
+        }, timeout);
+      
+        unlock = false;
+        setTimeout(function () {
+          unlock = true;
+        }, timeout);
+      }
+
+    function openPopup(id) {
+        bodyLock(); 
+        $(`.js-popup[data-id-popup='${id}']`).fadeIn(300);
+      }
+      
+      function closePopup() {
+        bodyUnlock();
+        $(".js-popup").fadeOut(300);
+      }
+      
+      $(".js-popup__close").click(closePopup);
+      
+      $(".js-btn-popup").click(function (e) {
+        e.preventDefault();
+        let indexBtnPopup = $(this).attr("href");
+
+        openPopup(indexBtnPopup);
+      });
+      
+      $(".js-popup").click(function (e) {
+        let popup = $(".js-popup__wrapp");
+        
+        if (!popup.is(e.target) && popup.has(e.target).length === 0) {
+          closePopup();
+        }
+      });
+      
+      /* === popup end === */
+
+      // input text in up
+
+      $('.requirements__input').focusin(function(e){
+        $(this).prev(".requirements__label").css("opacity", "1")
+      })
+
+      $('.requirements__input').focusout(function(e){
+        $(this).prev(".requirements__label").css("opacity", "0")
+      })
+
+      // sliders 
+
+      $('.result-slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true,
+        dots: true,
+    });
+
+    //rating 
+
+      const ratingItemsList = document.querySelectorAll('.rating__item');
+      const ratingItemsAray = Array.prototype.slice.call(ratingItemsList);
+
+      ratingItemsAray.forEach(item =>
+          item.addEventListener('click', () => {
+              const { itemValue } = item.dataset;
+              item.parentNode.dataset.totalValue = itemValue;
+          })
+      )
+
+    // scroll to block
+
+    // let heightHeader = $(".header").height();
+    $('.header__list li a').click(function(){
+        let element = $(this).attr('href');
+        // let dist = $(element).offset().top-heightHeader;
+        let dist = $(element).offset().top;
+
+        $('html, body').animate({'scrollTop': dist}, 500);
+
+        return false;
+    })
+
+});
+/*кнопка прокрутки вверх*/
+
+const offset = 100;
+const scrollUp = document.querySelector('.js-scroll-up');
+const scrollUpSvgPath = document.querySelector('.js-scroll-up__path');
+const pathLength = scrollUpSvgPath.getTotalLength();
+
+scrollUpSvgPath.style.strokeDasharray = '\'' + pathLength + pathLength + '\'';
+scrollUpSvgPath.style.transition = 'stroke-dashoffset 20ms';
+
+// getTop
+const getTop = () => window.pageYOffset || document.documentElement.scrollTop;
+
+//updateDashoffset
+
+const updateDashoffset = () => {
+    const heigth = document.documentElement.scrollHeight - window.innerHeight;
+    const dashoffset = pathLength - (getTop() * pathLength / heigth);
+
+    scrollUpSvgPath.style.strokeDashoffset = dashoffset;
+}
+
+// onScroll
+window.addEventListener('scroll', () => {
+    updateDashoffset();
+    getTop() > offset ? scrollUp.classList.add('scroll-up_active') : scrollUp.classList.remove('scroll-up_active');
+});
+
+// click
+scrollUp.addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth',
+    });
+});
+
+/*скрол по якорю*/
